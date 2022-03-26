2631c8be44ef9838055e67a9723347676b9d2d0b
Jason Carrillo <jason.carrillo182@gmail.com>
2022-03-26 17:09:14 -0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+ document.getElementById('showBox').addEventListener('click', function(){
+        document.getElementById('box').classList.remove('scale-0');
+        document.getElementById('box').classList.add('scale-100');
+    });
